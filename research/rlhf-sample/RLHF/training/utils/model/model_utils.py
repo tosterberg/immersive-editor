a3082ddae48da3cbb9d1c9f23cf9febc221759fe
Tyler Osterberg <osterberg614@gmail.com>
2023-08-06 13:05:39 -0700

    [research] rlhf part 3

--- research/rlhf-sample/RLHF/training/utils/model/model_utils.py
+++ research/rlhf-sample/RLHF/training/utils/model/model_utils.py
@@ -15,13 +15,17 @@ from transformers.deepspeed import HfDeepSpeedConfig
 from .reward_model import RewardModel
 
 
-def create_hf_model(model_class,
-                    model_name_or_path,
-                    tokenizer,
-                    ds_config=None,
-                    rlhf_training=False,
-                    disable_dropout=False):
-    model_config = AutoConfig.from_pretrained(model_name_or_path, trust_remote_code=True)
+def create_hf_model(
+    model_class,
+    model_name_or_path,
+    tokenizer,
+    ds_config=None,
+    rlhf_training=False,
+    disable_dropout=False,
+):
+    model_config = AutoConfig.from_pretrained(
+        model_name_or_path, trust_remote_code=True
+    )
     if disable_dropout:
         model_config.dropout = 0.0
     # Note: dschf is defined in function scope to avoid global effects
@@ -37,41 +41,48 @@ def create_hf_model(model_class,
         model = model_class.from_pretrained(
             model_name_or_path,
             from_tf=bool(".ckpt" in model_name_or_path),
-            config=model_config)
+            config=model_config,
+        )
 
     model.config.end_token_id = tokenizer.eos_token_id
     model.config.pad_token_id = model.config.eos_token_id
-    model.resize_token_embeddings(int(
-        8 *
-        math.ceil(len(tokenizer) / 8.0)))  # make the vocab size multiple of 8
+    model.resize_token_embeddings(
+        int(8 * math.ceil(len(tokenizer) / 8.0))
+    )  # make the vocab size multiple of 8
 
     return model
 
 
-def create_critic_model(model_name_or_path,
-                        tokenizer,
-                        ds_config,
-                        num_padding_at_beginning=0,
-                        rlhf_training=False,
-                        disable_dropout=False):
+def create_critic_model(
+    model_name_or_path,
+    tokenizer,
+    ds_config,
+    num_padding_at_beginning=0,
+    rlhf_training=False,
+    disable_dropout=False,
+):
     # OPT model family always put a padding token at the beginning of the sequence,
     # we did not see this in other models but not sure if it is a general rule
-    critic_model = create_hf_model(AutoModel, model_name_or_path, tokenizer,
-                                   ds_config, rlhf_training, disable_dropout)
-    critic_model = RewardModel(
-        critic_model,
+    critic_model = create_hf_model(
+        AutoModel,
+        model_name_or_path,
         tokenizer,
-        num_padding_at_beginning=num_padding_at_beginning)
+        ds_config,
+        rlhf_training,
+        disable_dropout,
+    )
+    critic_model = RewardModel(
+        critic_model, tokenizer, num_padding_at_beginning=num_padding_at_beginning
+    )
 
     if rlhf_training:
         if not os.path.isdir(model_name_or_path):
             model_name_or_path = snapshot_download(model_name_or_path)
         # critic model needs to load the weight here
-        model_ckpt_path = os.path.join(model_name_or_path, 'pytorch_model.bin')
+        model_ckpt_path = os.path.join(model_name_or_path, "pytorch_model.bin")
         assert os.path.exists(
             model_ckpt_path
         ), f"Cannot find model checkpoint at {model_ckpt_path}"
-        critic_model.load_state_dict(
-            torch.load(model_ckpt_path, map_location='cpu'))
+        critic_model.load_state_dict(torch.load(model_ckpt_path, map_location="cpu"))
 
     return critic_model
